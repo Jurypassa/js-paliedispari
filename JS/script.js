18c75511744e8ab24e0145acbe5a427043766420
Jury <jurypassa@gmail.com>
2021-11-16 15:21:09 +0100

    secondo esercizio

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+// ******Pari e Dispari --- 2° ESERCIZIO
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// const valore = prompt("Scegli se pari o dispari");
+// console.log(valore);
+
+// const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// console.log(numeroUtente);
+
+// function computer(min, max){
+//     return Math.floor(Math.random() * (max - min + min)) + min;
+// }
+
+// const numeroComputer = computer(1, 5);
+// console.log(numeroComputer);
+
+// const somma = numeroUtente + numeroComputer;
+// console.log(somma);
+
+// function vincitore(number){
+//     if(number % 2 == 0){
+//         return "pari"
+//     }
+
+//     return "dispari"
+// }
+
+// const esito = vincitore(somma);
+// console.log(esito);
+
+// if (valore == esito){
+//     console.log("Hai vinto");
+// }else{
+//     console.log("Hai perso");
+// }
+
+
+// ******Palidroma  --- 1° ESERCIZIO
+
+//Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma 
+
